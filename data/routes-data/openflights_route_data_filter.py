6ef0e65fe89655842bc9fc2835f3fd051ab4b677
Chen, Yuxuan <yuxuan19@illinois.edu>
2021-11-23 22:25:56 -0500

    Current progress with routes_data filter

---
+++ data/routes-data/openflights_route_data_filter.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+# input data.txt, output data.csv
+file = "routes.dat.txt"
+test = pd.read_csv(file, delimiter=",", encoding="utf_8")
+test.to_csv("./routes_data.csv", index=False, encoding="utf_8_sig")
+
+# obtain dataframe from data.csv
+file = "routes_data.csv"
+df = pd.read_csv(file)
+
+# check duplicates
+check_duplicates = df.duplicated()
+if True in check_duplicates.values:
+    df.drop_duplicates(inplace=True)
