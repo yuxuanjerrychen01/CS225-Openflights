f92bf23e65a1cf285a44089c50b46fb039d60bf9
Feng, Kangyu <kangyuf2@illinois.edu>
2021-11-25 17:55:32 -0600

    make change of test case
    change line 14 to test the new constructor

--- test.cpp
+++ test.cpp
@@ -2,23 +2,16 @@
  * this file is used to test the data
  * not related to the project
 */
+
+/* due to incorrect code, "make test" will not work until there is a change in "test.cpp" */
+/* usually make the change of number in "getInformation" */
 #include <fstream>
 #include <iostream>
 #include "Graph.cpp"
 using namespace std;
 int main()
 {
-    ifstream data("data/airports_data_useful.csv");     //try to read data from the csv
-    string line;                                        //store each line of data
-    vector<string> data2;
-    if(data) {
-        while (getline (data, line)){ 
-            data2.push_back(line);
-        }
-    } else {
-        cout <<"no such file" << endl;
-    }
-    Graph graph(data2);
-    cout << graph.getInformation(333) << endl;
+    Graph graph("data/airports_data_useful.csv");
+    cout << graph.getInformation(3332) << endl;
     return 0;
 }
