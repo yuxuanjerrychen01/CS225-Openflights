bcd464004b0bd8bcd0ec4b09834fb4cf42d9e63a
Yang, Ruozhen <ruozhen2@illinois.edu>
2021-11-25 17:58:02 -0600

    Update openflights_route_data_filter.py

--- data/routes-data/openflights_route_data_filter.py
+++ data/routes-data/openflights_route_data_filter.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 # input data.txt, output data.csv
-file = "routes.dat.txt"
+file = "routes_data.txt"
 test = pd.read_csv(file, delimiter=",", encoding="utf_8")
 test.to_csv("./routes_data.csv", index=False, encoding="utf_8_sig")
 
