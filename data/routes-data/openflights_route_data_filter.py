1ee15a5b28f2bb2c91b899e6a61cafa69af324d0
Yang, Ruozhen <ruozhen2@illinois.edu>
2021-11-25 18:34:24 -0600

    Update openflights_route_data_filter.py

--- data/routes-data/openflights_route_data_filter.py
+++ data/routes-data/openflights_route_data_filter.py
@@ -1,15 +1,44 @@
 import pandas as pd
 
-# input data.txt, output data.csv
 file = "routes_data.txt"
-test = pd.read_csv(file, delimiter=",", encoding="utf_8")
-test.to_csv("./routes_data.csv", index=False, encoding="utf_8_sig")
+test = pd.read_csv(file, delimiter = ",", encoding = "utf_8")
+test.to_csv("./routes_data.csv", index = False, encoding = "utf_8_sig")
 
-# obtain dataframe from data.csv
 file = "routes_data.csv"
 df = pd.read_csv(file)
 
-# check duplicates
 check_duplicates = df.duplicated()
 if True in check_duplicates.values:
-    df.drop_duplicates(inplace=True)
+  df.drop_duplicates(inplace=True)
+df
+
+df.columns
+#Index(['Airline (IATA or ICAO)', 'Airline ID', 'Source Airport',
+       #'Source Airport ID', 'Destination Airport', 'Destination Airport ID',
+       #'Codeshare', 'Stops', 'Equipment'],
+      #dtype='object')
+Airline_subset = df[['Airline (IATA or ICAO)']]
+Airline_subset.isna().sum()
+
+Airline_id_subset = df[['Airline ID']]
+Airline_id_subset.isna().sum()
+
+Airline_source_airport = df[['Source Airport']]
+Airline_source_airport.isna().sum()
+
+Airline_source_id = df[['Source Airport ID']]
+Airline_source_id.isna().sum()
+
+Airline_destination = df[['Destination Airport']]
+Airline_destination.isna().sum()
+
+Airline_destination_id = df[['Destination Airport ID']]
+Airline_destination_id.isna().sum()
+
+Airline_stops = df[['Stops']]
+Airline_stops.isna().sum()
+
+Airline_equipment = df[['Equipment']]
+Airline_equipment.isna().sum() #if we should drop the row with NaN equipment?
+#df['Equipment'].fillna('None', inplace = True)
+#Airline_equipment.isna().sum()
