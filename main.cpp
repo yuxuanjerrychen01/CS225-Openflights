601db67ee5a85eaa7052add05d68b016ac434048
Chen, Yuxuan <yuxuan19@illinois.edu>
2021-12-12 15:04:06 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,6 +11,98 @@
 
 int main() {
     
+//     //graph part
+//     Graph graph("data/airports_data_final_new.csv", "data/routes_data_final.csv");
+
+//     std::cout << "Hello! This is SPYE01000001 Openflights Data Analysis." << std::endl;
+//     std::cout << "get airport information -- enter 0." << std::endl;
+//     std::cout << "get shortest path between two airports -- enter 1." << std::endl;
+//     std::cout << "get important airports -- enter 2." << std::endl;
+//     std::cout << "get traversal txt -- enter 3." << std::endl;
+//     std::cout << "graph visualization of shortest path -- enter 4." << std::endl;
+//     std::cout << "graph visualization of destinations -- enter 5." << std::endl;
+
+//     int number;
+//     std::cin >> number;
+
+//     if (number < 0 || number > 5) {
+//         std::cout << "Not a valid choice! Must be an integer from 0 to 5." << std::endl;
+//         return 0;
+//     }
+
+//     if (number == 0) {
+//         std::cout << "Enter airport ID:" << std::endl;
+//         int airport_id;
+//         std::cin >> airport_id;
+//         std::vector<std::string> info = graph.getInformation(airport_id);
+//         if (info.size() == 1) {
+//             std::cout << info[0] << std::endl;
+//             return 0;
+//         }
+
+//         std::cout << "Airport name: " << info[1] << std::endl;
+//         std::cout << "Airport ID: " << info[0] << std::endl;
+//         std::cout << "City: " << info[2] << std::endl;
+//         std::cout << "Country: " << info[3] << std::endl;
+//         std::cout << "IATA: " << info[0] << std::endl;
+//         std::cout << "ICAO: " << info[0] << std::endl;
+//         std::cout << "Latitude: " << info[1] << std::endl;
+//         std::cout << "Longitude: " << info[1] << std::endl;
+//         return 0;
+//     }
+
+//     if (number == 1) {
+//         std::cout << "Enter source airport ID:" << std::endl;
+//         int source_airport_id;
+//         std::cin >> source_airport_id;
+
+//         std::cout << "Enter destination airport ID:" << std::endl;
+//         int dest_airport_id;
+//         std::cin >> dest_airport_id;
+
+//         std::vector<int> path = graph.Dijkstra(source_airport_id, dest_airport_id);
+//         //print the outcome of Dijkstra
+//         for (auto id : path) {
+//             std::vector<std::string> info = graph.getInformation(id);
+//             std::cout<< info[1] << " (" << info[0] << ") " << std::endl;
+//         }
+//         return 0;
+//     }
+
+//     if (number == 2) {
+//         return 0;
+//     }
+
+//     if (number == 3) {
+//         return 0;
+//     }
+
+//     if (number == 4) {
+//         std::cout << "Enter source airport ID:" << std::endl;
+//         int source_airport_id;
+//         std::cin >> source_airport_id;
+
+//         std::cout << "Enter destination airport ID:" << std::endl;
+//         int dest_airport_id;
+//         std::cin >> dest_airport_id;
+
+//         std::vector<int> path = graph.Dijkstra(source_airport_id, dest_airport_id);
+//         Draw draw("Equirectangular_projection_SW.png");
+//         draw.drawDijkstra(graph,path);
+//         return 0;
+//     }
+
+//     if (number == 5) {
+//         std::cout << "Enter airport ID:" << std::endl;
+//         int airport_id;
+//         std::cin >> airport_id;
+
+//         Draw draw("Equirectangular_projection_SW.png");
+//         draw.drawDestinations(graph, airport_id);
+//         return 0;
+//     }
+//     return 0;
+    
     //graph part
     Graph graph("data/airports_data_final_new.csv", "data/routes_data_final.csv");
     /**
